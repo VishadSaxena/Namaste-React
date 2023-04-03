eab2841ca942a0ed21ebab8fcd633ab8a3bcd53f
Vishad Saxena <vishad1710@gmail.com>
2023-04-03 15:07:12 +0530

    Update: Added React Components

--- App.js
+++ App.js
@@ -2,44 +2,18 @@
 import React from "react";
 
 import ReactDOM  from "react-dom/client";
-/*
 
-<div id = "babaji">
-    <div id = "tauji">
-        <div id="naman">
-            <h1>Naman is my name</h1>
-        </div>
+const Jsxheading = () =>
+     (<h1 className="Title">Namaste React through JSX</h1>
+     );
+
+const HeadingComponent = () => (
+    <div id="Heading">
+    <Jsxheading/> 
+    <h1>Namaste React through React Component</h1>
     </div>
-    <div id = "papaji">
-        <div id ="Varad">
-            <h1>Varad is my name</h1>
-        </div>
-         <div id = "Vishad">
-            <h1>Vishad is my name</h1>
-         </div>
-    </div>
-    <div id ="buaji">
-    </div>
-</div>
-
-
-*/
-
-
-
-const parent = React.createElement(
-    "div",
-    {id:"Babaji"},
-    [React.createElement("div",{id:"Papaji"},
-        [ React.createElement("h1",{id:"Varad"},"Varad is my name"),React.createElement("h2",{id:"Vishad"},"Vishad is my name")]
-    ),
-    React.createElement("div",{id:"Tauji"},
-         React.createElement("h3",{id:"Naman"},"Naman is my name")
-    ),
-    React.createElement("div",{id:"Buaji"},
-        React.createElement("h1",{},"Deepa Bua")
-    )]
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
